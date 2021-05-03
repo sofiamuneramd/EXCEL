913b1529a90539f4eac50ec5f6ecd2b8febf8b6e
Sofía Múnera <>
2021-05-03 23:26:13 +0000

    nnn

--- main.py
+++ main.py
@@ -4,41 +4,195 @@
 # Fuentes:
 
 
+# EJEMPLO 1
+
+# Importamos la libreria pandas para poder tratar archivos de Excel
+
 import pandas as pd
+
+# ExcelWriter lo usaremos para crear un nuevo archivo .xlsx
+
 from pandas import ExcelWriter
 
+# Importamos openpyxl para leer un archivo .xlsx
 
-class Excel:
+import openpyxl
+
+
+# Creamos la clase pais
+
+class Pais:
+
+  # Documentacion 
+
+  ''' Lee la informacion de un archivo .xls, la modifica y crea otro donde almacena los cambios '''
+
+  # Mediante __init__ crearemos los atributos de instancia 
 
   def __init__(self):
+
+    # Documentacion 
+
+    ''' Metodo constructor de la clase Pais, establece 3 atributos de instancia con valores predefinidos '''
+
+    # Creamos los atributos de instancia, sus valores serán cadenas de texto que representan 3 paises 
 
     self.pais0='España'
     self.pais1='Estados Unidos'
     self.pais2='China'
   
-  def leer(self):
+  # Creamos un metodo llamado de la clase pais llamado modificar 
 
-    libro1=pd.ExcelFile('Libro1.xls' )
+  def modificar(self):
 
-    hoja1=libro1.parse('Hoja1')
+    # Documentacion 
+
+    ''' Lee un archivo .xlsx, almacena la informacion que contiene y luego crea un nuevo archivo con esta informacion y otra adicional '''
+
+    # Usando la libreria openpyxl y la funcion load_workbook vamos a leer el archivo que usaremos de base llamado Libro1.xlsx
+
+    libro1=openpyxl.load_workbook('Libro1.xlsx')
+
+    # Ahora vamos a usar la Hoja1 del libro que acabamos de leer 
+
+    hoja1=libro1['Hoja1']
+
+    # Mediante Hoja1[celdas] vamos a leer los datos que usaremos en el archivo nuevo. De A2:A4 hay una lista de 3 capitales y de B2:B4 estan los idiomas que hablan en cada una de estas ciudades
+
+    capitales=hoja1['A2':'A4']
+    idiomas=hoja1['B2':'B4']
+
+
+    # Ahora comenzaremos a crear un nuevo libro
+
+    # Creamos una lista con los atributos de instancia
 
     paises=[self.pais0,self.pais1,self.pais2]
 
-    capitales=hoja1['CAPITAL'].values
-
-    idiomas=hoja1['IDIOMA'].values
+    # Mediante DataFrame crearemos una tabla donde las cabeceras de cada columna son Pais,Capital e Idioma y a cada uno de estos encabezados le vamos a asignar unos datos. En el caso de Capital e Idioma los datos serán los que leimos del archivo original (Libro1) y para Pais se le signa la lista que contiene los valores de los atributos de instancia 
 
     copia1=pd.DataFrame({'Pais':paises,'Capital':capitales,'Idioma':idiomas})
 
+    # Crearemos una nueva lista con informacion de continentes 
+
     continentes=['Europa','América','Asia']
+
+    # Insertaremos una columna con encabezado Continente y valores de la lista continentes. Esta irá en la posicion/locacion 2, lo que significará que la columna Idioma se desplazará una a la derecha 
+
+    # el orden de columnas será Pais, Capital, Continente y por ultimo Idioma
 
     copia1.insert(2,'Continente',continentes,allow_duplicates=False)
 
-    archivo=ExcelWriter('copia1.xls')
+    # mediante Excel writer crearemos un archivo llamado Copia1.xlsx
+
+    archivo=ExcelWriter('Copia1.xlsx')
+
+    # Ahora con ayuda de .to_excel el Dataframe que creamos (incluyendo la informacion insertada posteriormente) lo guardaremos en este nuevo archivo (Copia1), la hoja se llamara Hoja Copia y podemos index=False para que no se incluya encabezado de numeros 
+
     copia1.to_excel(archivo,'Hoja Copia',index=False)
+
+    # Guardamos lo ingresado al archivo copia 1
+
     archivo.save()
+
+    # Cerramos el archivo 
+
     archivo.close()
 
+# Llamamos la clase Pais, que inmediantamente creará los 3 atributos de instancia 
 
-a=Excel()
-a.leer()
+a=Pais()
+
+# Mediante el metodo modificar vamos a leer la información de Libro1 y crearemos un nuevo archivo llamado Copia 1 
+
+a.modificar()
+
+# FINALIZA EJEMPLO 1
+
+
+# EJEMPLO 2
+
+class Pais:
+
+  def __init__(self):
+
+    self.encabezado0='PAIS'
+    self.encabezado1='POBLACIÓN'
+  
+  def agregar(self):
+
+    # Documentacion 
+
+    ''' Lee un archivo .xlsx, almacena la informacion que contiene y luego crea un nuevo archivo con esta informacion y otra adicional '''
+
+    # Usando la libreria openpyxl y la funcion load_workbook vamos a leer el archivo que usaremos de base llamado Libro1.xlsx
+
+    libro1=openpyxl.load_workbook('Libro2.xlsx')
+
+    # Ahora vamos a usar la Hoja1 del libro que acabamos de leer 
+
+    hoja1=libro1['Hoja1']
+
+    # Mediante Hoja1[celdas] vamos a leer los datos que usaremos en el archivo nuevo. De A2:A4 hay una lista de 3 capitales y de B2:B4 estan los idiomas que hablan en cada una de estas ciudades
+
+    paises=hoja1['A2':'A4']
+    poblacion_total=hoja1['B2':'B4']
+
+    copia2=pd.DataFrame({self.encabezado0:paises,self.encabezado1:poblacion_total})
+
+    archivo=ExcelWriter('Copia2.xlsx')
+
+    # Ahora con ayuda de .to_excel el Dataframe que creamos (incluyendo la informacion insertada posteriormente) lo guardaremos en este nuevo archivo (Copia1), la hoja se llamara Hoja Copia y podemos index=False para que no se incluya encabezado de numeros 
+
+    copia2.to_excel(archivo,'Hoja1',index=False)
+
+    # Guardamos lo ingresado al archivo copia 1
+
+    archivo.save()
+
+    # Cerramos el archivo 
+
+    archivo.close()
+
+  def estadisticas():
+
+    # Usando la libreria openpyxl y la funcion load_workbook vamos a leer el archivo que usaremos de base llamado Libro1.xlsx
+
+    libro1=openpyxl.load_workbook('Libro2.xlsx')
+
+    # Ahora vamos a usar la Hoja1 del libro que acabamos de leer 
+
+    hoja1=libro1['Hoja1']
+
+    descripcion=hoja1['POBLACION'].describe()
+
+    copia2=pd.DataFrame({'ESTADISTICAS':descripcion})
+
+    archivo=ExcelWriter('Copia2.xlsx')
+
+    # Ahora con ayuda de .to_excel el Dataframe que creamos (incluyendo la informacion insertada posteriormente) lo guardaremos en este nuevo archivo (Copia1), la hoja se llamara Hoja Copia y podemos index=False para que no se incluya encabezado de numeros 
+
+    copia2.to_excel(archivo,'Hoja2',index=False)
+
+    # Guardamos lo ingresado al archivo copia 1
+
+    archivo.save()
+
+    # Cerramos el archivo 
+
+    archivo.close()
+
+a=Pais()
+a.estadisticas
+a.agregar()
+
+
+
+
+
+
+
+
+
+
+
